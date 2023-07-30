80b4e91f932b9c8b32a48480e8b9159106b59a38
Genplat <genplat@elite6-27.cf>
2023-07-30 23:33:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -166,9 +166,9 @@ function check_dependencies() {
     rm /data/data/com.termux/files/usr/etc/profile.d/init-termux-properties.sh # This fix bugs
     rm /data/data/com.termux/files/usr/etc/motd # This file bug some devices :/
     pkg update -y
-    apt update -y
     rm /data/data/com.termux/files/usr/etc/apt/sources.list # Idk why but deleting this, I fix a bug
     pkg i apt
+    apt update -y
     apt-get update -y|| apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" dist-upgrade -y
     pkg -y i nano python python3 figlet toilet git zsh neovim wget curl zip 
     pip3 install lolcat
