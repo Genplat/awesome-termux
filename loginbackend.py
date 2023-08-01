9a611b422c2020278fe2d1eb3b96736589287db9
Genplat <genplat@elite6-27.cf>
2023-08-01 12:57:16 +0200

    Update loginbackend.py

--- loginbackend.py
+++ loginbackend.py
@@ -1,19 +1,26 @@
 import time
 import os
+
 def login(username, passwd):
-    usernameinfile = open("/data/data/com.termux/files/usr/etc/usrtermux")
-    passwdinfile = open("/data/data/com.termux/files/usr/etc/passwdtermux")
-    if username == usernameinfile and passwd == passwdinfile:
+    with open("/data/data/com.termux/files/usr/etc/usrtermux") as usernameinfile:
+        stored_username = usernameinfile.read().strip()
+    with open("/data/data/com.termux/files/usr/etc/passwdtermux") as passwdinfile:
+        stored_passwd = passwdinfile.read().strip()
+
+    print(f"Pass: {stored_passwd} y User: {stored_username}")
+
+    if username == stored_username and passwd == stored_passwd:
         os.system('touch /data/data/com.termux/files/home/custoMUX/passwdlock')
         os.system("mainmenu")
     else:
         print("[!] ERROR.")
-        time.sleep(3) 
+        time.sleep(3)
         exit()
+
 try:
-	file = open('/data/data/com.termux/files/home/custoMUX/passwdlock')
-	file.close()
-	os.system("mainmenu")
+    file = open('/data/data/com.termux/files/home/custoMUX/passwdlock')
+    file.close()
+    os.system("mainmenu")
 except FileNotFoundError:
     username = input("User: ")
     passwd = input("Password: ")
